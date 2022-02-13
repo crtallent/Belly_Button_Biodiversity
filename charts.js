7dc92f54eacc0228d7d80ef22f3074ef107fb331
crtallent <91101467+crtallent@users.noreply.github.com>
2022-02-13 15:40:52 -0600

    Updated charts.js.

--- charts.js
+++ charts.js
@@ -82,7 +82,7 @@ function buildCharts(sample) {
      otuIds.sort((a,b) => b.otuIds - a.otuIds);
      
      var yticks = otuIds.slice(0, 10);
-     
+  
 
     8. // Create the trace for the bar chart. 
      var barData = [{
@@ -104,4 +104,28 @@ function buildCharts(sample) {
     
   });
 
+}
+// Bar and Bubble charts
+// Create the buildCharts function.
+function buildCharts(sample) {
+  // Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    
+
+    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot(); 
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot(); 
+  });
 }
